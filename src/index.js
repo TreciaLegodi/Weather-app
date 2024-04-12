25e68a6bfe11a03ed74d36af9e4dfb45d121c2b5
TreciaLegodi <162156337+TreciaLegodi@users.noreply.github.com>
2024-04-12 20:41:26 +0200

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -3,9 +3,49 @@ function refreshWeather(response) {
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
 
-  cityElement.innerHTML = response.data.city;
+  let weatherConditionElement = document.querySelector("#weather-condition");
+  let condition = response.data.condition.description;
 
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let windSpeed = response.data.wind.speed;
+
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  windSpeedElement.innerHTML = `${windSpeed}km/h`;
+  humidityElement.innerHTML = `${humidity}%`;
+  weatherConditionElement.innerHTML = condition;
   temperatureElement.innerHTML = temperature;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
